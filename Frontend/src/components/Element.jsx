1b4634bddd17eb9e57f0b46424503829729bd35a
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2021-09-03 14:47:46 +0530

    same

--- Frontend/src/components/Element.jsx
+++ Frontend/src/components/Element.jsx
@@ -1,21 +1,30 @@
-import React from 'react';
+import React ,{useEffect} from 'react';
 import {Link} from "react-router-dom";
-
+// import { fadeInDown } from 'react-animations';
+import { animations } from 'react-animation'
+// import { StyleSheet, css } from 'aphrodite';
+import 'react-animation/dist/keyframes.css';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 const Element= (props)=>{
+
+    // 
+    useEffect(() => {
+        Aos.init({duration: 2000});
+    }, []);
+
     return (
         <>
-        
-            <div className="col-md-4 col-10 mx-auto">
+            <div className="col-md-4 col-10 mx-auto" style={{animation: animations.popIn}}>
                 <div class="card" style={{ boxShadow: "0 8px 8px -4px lightblue"}} >
+                <div class="card-body">
                     <img src={props.imgsrc} class="card-img-top" alt={props.imgsrc} />
-                        <div class="card-body">
                             <h5 class="card-title font-weight-bold">{props.title}</h5>
                             <p class="card-text">Time to ready : {props.time} minutes</p>
-                            <a type="botton" href={props.recipe} class="btn btn-outline-dark" style={{marginLeft: "31%"}}>Show recipe</a>
+                            <a type="botton" href={props.recipe} class="btn btn-outline-dark" style={{width: "100%"}}>Show recipe</a>
                         </div>
                 </div>
             </div>
-
                    
         </>
     );
--- Frontend/src/components/FinalHome.jsx
+++ Frontend/src/components/FinalHome.jsx
@@ -31,10 +31,9 @@ const FinalHome = (props) => {
     {/* <h1> {props.location.state.details.first_name}</h1> */}
     <section id="header" className="d-flex align-items-center">
     <div id="title" className="titleHeader col-md-6 pt-5 pt-lg-0 order-2 order-lg- d-flex justify-content-center flex-column" >
-                <h1>Welcome to the world of <strong className="brand-name"> <ReactTextTransition text={texts[index % texts.length]}
-              springConfig={presets.wobbly}
-              style={{ margin: "0 4px" }}
-              inline />
+                <h1>Welcome to the world of <strong className="brand-name"> 
+                <Typewriter options={{  strings: ["Recipes", "Magic", "Taste"],  autoStart: true,  loop: true,changeDelay:2 }}/>
+
               </strong>
                </h1>
                <h2 className="my-3" style={{color:"white"}}>
