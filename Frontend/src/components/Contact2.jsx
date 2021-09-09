c1d663c8bcafcd1ea446da78f6cae98cbed44903
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2021-09-09 13:59:11 +0530

    final touch

---
+++ Frontend/src/components/Contact2.jsx
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+import Wave from 'react-wavify';
+import swal from 'sweetalert';
+
+
+const Contact2 =  ()=>{
+    return <>
+    </>
+}
+
+export default Contact2;
