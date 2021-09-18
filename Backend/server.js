4613520dccfeb24c20bfb8a62d6c6b3267a639fc
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2021-09-19 02:16:14 +0530

    Font resizing

--- Backend/server.js
+++ Backend/server.js
@@ -11,6 +11,28 @@ const cors = require("cors");
 const errorHandler = require("./utils/errorHandler");
 
 const app = express();
+
+const knex = require('knex')({
+  client: 'mysql',
+  
+  connection: {
+    host : process.env.DB_HOST,
+    user : process.env.DB_USER,
+    password : process.env.DB_PASS,
+    database : process.env.DB_NAME,
+  },
+  pool: { min: 0, max: 50 },
+});
+
+knex.connect();
+
+
+
+
+
+
+
+
 app.use(cors());
 
 app.use(override.overrideContentType());
