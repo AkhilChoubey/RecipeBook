9ae40b339fef6e6f81ccd159e547a076f89f80e7
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2021-09-19 14:33:32 +0530

    Backend bug fixing

--- Backend/config/knex.js
+++ Backend/config/knex.js
@@ -3,10 +3,10 @@ const knex = require('knex')({
     port: process.env.MYSQL_ADDON_PORT,
     connection: {
       host :  'bz8gpu5jgvbjqlg1yvio-mysql.services.clever-cloud.com',// process.env.MYSQL_ADDON_HOST,
-      user : process.env.MYSQL_ADDON_USER,
-      password : process.env.MYSQL_ADDON_PASSWORD,
-      database : process.env.MYSQL_ADDON_DB,
-      socketPath : process.env.MYSQL_ADDON_URI
+      user :   "uxajjrg7gc78343d",//process.env.MYSQL_ADDON_USER,
+      password : "njZocbMsQTbcqQY5qGtu" ,//process.env.MYSQL_ADDON_PASSWORD,
+      database :  "bz8gpu5jgvbjqlg1yvio",//process.env.MYSQL_ADDON_DB,
+     // socketPath :   //process.env.MYSQL_ADDON_URI
     },
     pool: { min: 0, max: 50 },
 });
