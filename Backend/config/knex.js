e0dd9fe8d25b1c55089f01c05e0a5c3c59a9e000
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2021-09-19 12:34:04 +0530

    Backend bug fixing

--- Backend/config/knex.js
+++ Backend/config/knex.js
@@ -2,10 +2,10 @@ const knex = require('knex')({
     client: 'mysql',
     port: '3306',
     connection: {
-      host : process.env.DB_HOST,
-      user : process.env.DB_USER,
-      password : process.env.DB_PASS,
-      database : process.env.DB_NAME,
+      host : process.env.MYSQL_ADDON_HOST,
+      user : process.env.MYSQL_ADDON_USER,
+      password : process.env.MYSQL_ADDON_PASSWORD,
+      database : process.env.MYSQL_ADDON_DB,
     },
     pool: { min: 0, max: 50 },
 });
