dd3160f4e07285b83441a4189ba3b7b014d38bf2
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2021-09-19 12:09:09 +0530

    Font resizing

--- Backend/server.js
+++ Backend/server.js
@@ -20,7 +20,7 @@ app.use(cors({origin: 'https://akhilrecipeapp.netlify.app'}));
 
 
 
-app.use(cors());
+app.use(cors({origin: 'https://akhilrecipeapp.netlify.app'}));
 
 app.use(override.overrideContentType());
 app.use(express.json({ limit: "50mb", extended: true, parameterLimit: 50000 }));
